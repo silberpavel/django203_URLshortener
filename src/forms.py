f600e1d237bcf6e81b2b099e63567cace1ef2e80
Silber <silber.ypm@gmail.com>
2018-03-12 14:00:52 +0200

    clean method added (Form Validation & Custom Validators)
    Form Validation & Custom Validators

--- src/forms.py
+++ src/forms.py
@@ -3,3 +3,7 @@ from django import forms
 
 class SubmitUrlForm(forms.Form):
     url = forms.CharField(label='Submit URL')
+
+    def clean(self):
+        cleaned_data = super(SubmitUrlForm, self).clean()
+        print(cleaned_data)
