66b42256abe7073e814c5ad659d0c860cf839ead
Silber <silber.ypm@gmail.com>
2018-03-12 15:54:07 +0200

    validation without http://

--- src/shortener/validators.py
+++ src/shortener/validators.py
@@ -4,9 +4,20 @@ from django.core.exceptions import ValidationError
 
 def validate_url(value):
     url_validator = URLValidator()
+    value_1_invalid = False
+    value_2_invalid = False
     try:
         url_validator(value)
     except:
+        value_1_invalid = True
+
+    value_2_url = "http://" + value
+    try:
+        url_validator(value_2_url)
+    except:
+        value_2_invalid = True
+    value_2_url = "http://" + value
+    if value_1_invalid == False and not value_2_invalid == False:
         raise ValidationError("Invalid URL for this field")
     return value
 
