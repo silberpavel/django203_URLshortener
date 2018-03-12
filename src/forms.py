c1d481433467b587613fe2e5e1fd02e54ffd32ab
Silber <silber.ypm@gmail.com>
2018-03-12 14:18:50 +0200

    URL validator creadet in forms.py

--- src/forms.py
+++ src/forms.py
@@ -1,9 +1,20 @@
 from django import forms    
-
+from django.core.validators import URLValidator
 
 class SubmitUrlForm(forms.Form):
     url = forms.CharField(label='Submit URL')
 
     def clean(self):
         cleaned_data = super(SubmitUrlForm, self).clean()
-        print(cleaned_data)
+        # url = cleaned_data["url"]
+        # print(url)
+
+    def clean_url(self):
+        url = self.cleaned_data["url"]
+        print(url)
+        url_validator = URLValidator()
+        try:
+            url_validator(url)
+        except:
+            raise forms.ValidationError("Invalid URL for this field")
+        return url
